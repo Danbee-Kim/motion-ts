48893edb689f4adc40ec25fca015f74130a0ecd9
Danbee Kim <kdb2360@gmail.com>
2022-12-23 12:23:44 +0900

    모든 섹션들 구현

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import { ImageComponent } from "./components/page/item/image.js";
+import { NoteComponenet } from "./components/page/item/note.js";
+import { TodoComponenet } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 import { PageComponent } from "./components/page/page.js";
 class App {
   private readonly page: PageComponent;
@@ -11,6 +14,18 @@ class App {
       "https://picsum.photos/600/300",
     );
     image.attachTo(appRoot, "beforeend");
+
+    const note = new NoteComponenet("note Title", "note body");
+    note.attachTo(appRoot, "beforeend");
+
+    const todo = new TodoComponenet("todo Title", "todo item");
+    todo.attachTo(appRoot, "beforeend");
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://www.youtube.com/watch?v=Lk4e1mk4lJU",
+    );
+    video.attachTo(appRoot, "beforeend");
   }
 }
 
